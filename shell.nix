88a30f0caf74e5bbcb7da9afef4f7b501b8fa68c
katharye <multiden007@gmail.com>
2026-07-12 17:48:51 +0300

    nix-shell config

---
+++ shell.nix
@@ -0,0 +1,52 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    python313
+    python313Packages.pip
+    python313Packages.setuptools
+    python313Packages.wheel
+    rustc
+    cargo
+
+    pkg-config
+    openssl
+    libffi
+    libxml2
+    libxslt
+    zlib
+  ];
+
+  shellHook = ''
+    export PIP_BREAK_SYSTEM_PACKAGES=1
+
+    FIRST_TIME=false
+
+    if [ -z "$VIRTUAL_ENV" ]; then
+      if [ ! -d ".venv" ]; then
+        echo "Creating Python virtual environment (.venv)..."
+        python -m venv .venv
+        FIRST_TIME=true
+      fi
+
+      source .venv/bin/activate
+    fi
+
+    export PKG_CONFIG_PATH=${pkgs.lib.makeSearchPath "lib/pkgconfig" [
+      pkgs.openssl
+      pkgs.libffi
+      pkgs.libxml2
+      pkgs.libxslt
+    ]}
+    
+    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
+      pkgs.stdenv.cc.cc
+      pkgs.openssl
+      pkgs.zlib
+    ]}:$LD_LIBRARY_PATH
+
+    if [ "$FIRST_TIME" = true ]; then
+      pip install aiogram python-dotenv aiohttp bs4
+    fi
+  '';
+}
