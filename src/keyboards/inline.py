b0b21957d2f8263e40844f98d90801c5663ea8a4
katharye <multiden007@gmail.com>
2026-07-14 10:49:29 +0300

    add keyboard to сщташкь reset

--- src/keyboards/inline.py
+++ src/keyboards/inline.py
@@ -7,6 +7,28 @@ def generateGroupsKeyboard(groups: list[str]) -> InlineKeyboardMarkup:
 
     for group in groups:
         groupsKeyboard.button(text=group, callback_data=f"group_{group}")
-    groupsKeyboard.adjust(4)
+    groupsKeyboard.button(
+        text="Назад", 
+        callback_data="SETTUP_BACK", 
+        style="danger",
+        icon_custom_emoji_id="5854967531793550989")
 
+    buttons_count = len(groups)
+    rowsOfFour = buttons_count // 4
+    remainder = buttons_count % 4
+    adjust_shema = [4] * rowsOfFour
+    adjust_shema.append(remainder)
+
+    groupsKeyboard.adjust(*adjust_shema)
     return groupsKeyboard.as_markup()
+
+def applyResetKeyboard() -> InlineKeyboardMarkup:
+    resetKeyboard = InlineKeyboardBuilder()
+    resetKeyboard.button(text="Да", callback_data="RESET_YES", 
+                         style="success", icon_custom_emoji_id="5373084673767407538")
+    resetKeyboard.button(text="Нет", callback_data="RESET_NO", 
+                         style="danger", icon_custom_emoji_id="5375142362534148649")
+    return resetKeyboard.as_markup()
+
+
+# КАЛЕНДАРЬ 52516137687517977722
