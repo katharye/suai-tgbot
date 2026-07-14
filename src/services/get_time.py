0885236581e39bb4522d852873646df0510db095
ivan <d1zzzy.project@gmail.com>
2026-07-14 12:47:25 +0300

    add-parse-time

---
+++ src/services/get_time.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+def get_time_notifications_one_session(strr: str)->int | None: # в секундах
+    # ЧЧ ММ
+    # НАПИШИТЕ В ФОРМАТЕ ....
+    # ЧЧ:ММ (пример: 01:20)
+    
+    try:
+        t = strr.split(':')
+        return int(t[0]) * 3600 + int(t[1]) * 60
+    except Exception:
+        return None
+
+def get_time_notifications_all_session(strr: str)->int | None:
+    try:
+        res_time = datetime.strptime(strr, "%H:%M").time()
+        return res_time
+    except Exception:
+        return False
+
+print(get_time_notifications_all_session('23:59'))
