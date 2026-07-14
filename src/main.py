60022253eb20e21573552ab3102de5e0b734283e
katharye <multiden007@gmail.com>
2026-07-14 10:52:15 +0300

    add database, admin and registration routers

--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
 import asyncio
-from aiogram import Dispatcher, Router, F, Bot
+from aiogram import Dispatcher, Bot
 
 from config_data.config import getToken
 from handlers.user import user_router
+from handlers.admin import admin_router
+from handlers.registration import registration_router
+from database.middleware import DbSessionMiddleware
+
 async def main():
     bot = Bot(token=getToken())
     dp = Dispatcher()
 
+    dp.include_router(registration_router)
     dp.include_router(user_router)
+    dp.include_router(admin_router)
 
+    #database
+    dp.update.middleware(DbSessionMiddleware())
+    
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
